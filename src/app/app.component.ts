25ac0932b627046c23d7e961abd36fa443e75853
hossam <hoss.salah199@gmail.com>
2024-09-12 08:29:35 +0200

    protofil

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
+import { HomeComponent } from "./home/home.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { AboutmeComponent } from "./aboutme/aboutme.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, ProjectsComponent, ContactComponent, HomeComponent, SkillsComponent, AboutmeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
 }
+
